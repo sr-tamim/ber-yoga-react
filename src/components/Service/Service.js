bb3d793b6ca24d9a35d9935d092540004262d93e
TAMIM <saifur.tamim@gmail.com>
2021-10-04 19:32:19 +0600

    service card animation

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -4,17 +4,17 @@ import "./Service.css";
 const Service = props => {
     const { name, thumbnail, classAmount, daysNeeded, Level, duration, price } = props.service;
     return (
-        <div>
-            <div className="col px-5 py-4">
+        <div className="service-container p-5">
+            <div className="service col position-relative">
                 <div className="card border-0">
                     <img src={thumbnail} className="card-img-top" alt="..." />
 
-                    <div className="card-body">
+                    <div className="card-body p-4">
                         <h5 className="card-title">{name}</h5>
                         <p className="card-text text-secondary">{daysNeeded} days | {classAmount} classes | {Level} </p>
-                        <div className="d-flex justify-content-between align-items-baseline">
-                            <h4 className="my-0">{duration}</h4>
-                            <h3 className="my-0">${price}</h3>
+                        <div className="d-flex justify-content-between align-items-baseline mb-3">
+                            <h5 className="my-0">{duration}</h5>
+                            <h3 className="my-0 text-danger fw-bold">${price}</h3>
                         </div>
                     </div>
                 </div>
