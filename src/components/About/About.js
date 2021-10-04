88da1e163cc8f108083b1cf464a71e265c0c4241
TAMIM <saifur.tamim@gmail.com>
2021-10-04 18:54:43 +0600

    about us page finished

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./About.css";
 
 const featursContainerStyles = {
     background: "url(./img/flower.png), url(./img/abstract-1.png)",
@@ -7,10 +8,25 @@ const featursContainerStyles = {
 }
 const About = () => {
     return (
-        <div className="my-5 py-5">
-            <h1 className="display-1 text-center my-5">About Us</h1>
+        <div className="my-5 py-4">
+            <h1 className="display-1 text-center text-danger my-5">About Us</h1>
 
             <section id="features" style={featursContainerStyles}>
+                <div id="ourMission" className="container row mx-auto py-5">
+                    <p className="col-12 col-xl-7 fs-4 p-5 d-flex flex-column justify-content-center">
+                        <span className="text-danger display-4 fw-bold">We're on a mission</span> <br />
+                        <span className="text-danger fs-2">to make yoga and mindfulness fun!</span> <br />
+                        <p style={{ textAlign: 'justify' }}>
+                            Every week, millions of peoples do our yoga and mindfulness sessions on video in class and at home. And thousands of teachers that we've trained lead classes in their own communities. We're doing what we can to make yoga and mindfulness fun for every people - so they can enjoy the physical, mental and emotional benefits.
+                        </p>
+                    </p>
+                    <div className="col-12 col-xl-5 col-xl-5 d-flex flex-column justify-content-center align-items-center bg-danger text-white p-5 position-relative">
+                        <span className="display-1 fw-bold m-5 mb-0">YOGA</span> <br />
+                        <span className="display-3 fw-bold">HEALTH</span> <br />
+                        <span className="display-5 fw-bold m-5 mt-0">STRENGTH</span>
+                    </div>
+                </div>
+
                 <div className="container row mx-auto py-5">
                     <div className="col-12 col-lg-6 p-4">
                         <img className="img-fluid" src="./img/yoga-woman.png" alt="" />
@@ -48,6 +64,7 @@ const About = () => {
                         </div>
                     </div>
                 </div>
+
             </section>
 
         </div>
